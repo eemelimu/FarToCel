e4e6c047b05e26b28156a72d6e8c59cde729b8b9
eemelimu <ebe.22@hotmail.com>
2024-02-23 17:08:00 +0200

    Update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,12 +8,4 @@ public class App {
     public static float kelvinToCelsius(float kelvin) {
         return (float) (kelvin - 273.15);
     }
-
-    public static void main( String[] args ){
-        int fahrenheit2celcius = new App().toCelsius(32);
-        System.out.println("32 Fahrenheit is " + fahrenheit2celcius + " Celsius");
-
-        float kelvin2celcius = App.kelvinToCelsius(273.15f);
-        System.out.println("273.15 Kelvin is " + kelvin2celcius + " Celsius");
-    }
 }
