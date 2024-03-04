2f9694a696a7a042122ae7f5135b3d889fd7eef8
eemelimu <ebe.22@hotmail.com>
2024-03-04 12:27:46 +0200

    Update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,7 @@ public class App {
         return (float) (kelvin - 273.15);
     }
 
+
     public static float KelToFar(float kelvin) {
         return (float) ((kelvin -273.15) * 9/5 + 32);
     }
